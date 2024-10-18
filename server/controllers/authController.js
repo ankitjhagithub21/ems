c0853b80dfec68fc414b8b29cc229c8a1d2f1d86
ankitjhagithub21 <ajha45075@gmail.com>
2024-10-18 20:18:03 +0530

    added login logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,32 +1,62 @@
-const register = async(req,res) =>{
-    try{
+const User = require("../models/User")
+const bcryptjs = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
-    }catch(error){
-        return res.status(500).json({message:"Server error"})
+const register = async (req, res) => {
+    try {
+
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" })
     }
 }
 
-const login = async(req,res) =>{
-    try{
+const login = async (req, res) => {
+    try {
 
-    }catch(error){
-        return res.status(500).json({message:"Server error"})
+        const { email, password } = req.body;
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(404).json({ message: "user not found." })
+        }
+
+
+        const validPassword = await bcryptjs.compare(password, user.password)
+
+        if (!validPassword) {
+            return res.status(404).json({ message: "Wrong email or password." })
+        }
+
+        const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" })
+
+        res.status(200).json({
+            message: "Login successufll.", token,
+            user: {
+                _id: user._id,
+                role: user.role,
+                name: user.name
+            }
+        })
+
+
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" })
     }
 }
 
-const logout = async(req,res) =>{
-    try{
+const logout = async (req, res) => {
+    try {
 
-    }catch(error){
-        return res.status(500).json({message:"Server error"})
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" })
     }
 }
 
-const getUser = async(req,res) =>{
-    try{
+const getUser = async (req, res) => {
+    try {
 
-    }catch(error){
-        return res.status(500).json({message:"Server error"})
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" })
     }
 }
 
