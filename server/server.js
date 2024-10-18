60307e682edb71ad4ffcee8017b84cb6d386feae
ankitjhagithub21 <ajha45075@gmail.com>
2024-10-18 19:27:31 +0530

    connect database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const connectDb = require('./db/conn')
 const app = express()
-const port = process.env.PORT || 3000
 
+const port = process.env.PORT || 3000
+connectDb()
 
 app.use(express.json())
 app.use(cors())
