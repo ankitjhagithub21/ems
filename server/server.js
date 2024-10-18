48e30907f76e0ebe5c12a2ac496eef8375e910d8
ankitjhagithub21 <ajha45075@gmail.com>
2024-10-18 19:19:49 +0530

    server setup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
+require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+
+app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json({
+    "message":"Api working."
+  })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server is listening on port ${port}`)
 })
